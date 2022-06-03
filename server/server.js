2d2654badfbb8e6d383adf5cf6f97e7fb9633b81
Joseph Gabereau <jgabereau@gmail.com>
2022-06-03 12:03:41 -0400

    calling occupants

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Calling occupants");
+  res.send("Calling occupants of interplanetary craft");
 });
 
 app.listen(8000);
